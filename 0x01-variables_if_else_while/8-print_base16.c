270dabfd4621e0140f28ecf91b43fa3b552add1a
Lakhanya <lakhanya.mahlati@gmail.com>
2022-04-08 17:12:55 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char base_16;
+	
+	for (base_16 = 48; base <= 57;  base 16++)
+	{
+		putchar(base_16);
+	}
+	
+	for (base_16 = 97; base_16 <= 102; base_16++)
+	{
+		putchar(base_16)
+	}
+
+	putchar ('\n');
+
+	return (0);
+}		
