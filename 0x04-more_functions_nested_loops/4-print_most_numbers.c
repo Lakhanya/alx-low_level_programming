5c2442408a7ed8dac74c848d099131588d175f7c
Lakhanya <lakhanya.mahlati@gmail.com>
2022-04-14 11:38:45 -0700

    Task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,16 +12,16 @@ void print_most_numbers(void)
 
 	i = 0;
 
-	for (i = 0, i < 10, i++)
+	while (i < 10)
 	{
 
-		if (i == 2 && i == 4)
-
-			i++;
-
+		if (i != 2 && i != 4)
+		{
 		_putchar(i + '0');
-
+		}
+		i++;
 	}
+
 	_putchar('\n');
 
 }
