c902ce052a46e4984305f235888e98d5695a513e
Lakhanya <lakhanya.mahlati@gmail.com>
2022-05-03 09:17:04 -0700

    Create a static Library

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: a pointer to int that will be changed
+ *
+ * Return: void (success)
+ */
+
+void _puts(char *str)
+{
+	char *c;
+
+	int m;
+
+	c = str;
+
+	for (m = 0; c[m]; m++)
+	{
+		_putchar (c[m]);
+	}
+	_putchar('\n');
+}
