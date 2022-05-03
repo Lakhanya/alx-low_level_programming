ccd6dfaa019615e1ffed5a0f17f0ef2b822da831
Lakhanya <lakhanya.mahlati@gmail.com>
2022-05-03 08:06:30 -0700

    Task 0

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strchr - locates charcater in a string
+ * @s: string to check
+ * @c: charcater to be located
+ *
+ * Return: a pointer to the first occurance of char @c in string @s,
+ * return NULL if the character isn't found
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s != c)
+			s++;
+		else
+			return (s);
+	}
+	if (c == '\0')
+		return (s);
+
+	return (NULL);
+}
