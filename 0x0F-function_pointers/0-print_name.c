d92d79c98646c08dbbac52dfe3a6fe3cf2c86863
Lakhanya <lakhanya.mahlati@gmail.com>
2022-05-11 13:56:50 -0700

    print name from function pointer

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,9 +4,12 @@
  * print_name - Print name from function pointer
  * @name: char string
  * @f: function pointer that takes a string argument
+ * Return: nothing
  */
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (!name || !f)
+		return;
 	f(name);
 }
