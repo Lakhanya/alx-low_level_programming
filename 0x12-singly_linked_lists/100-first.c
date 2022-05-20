08fb5aab7dbe16ee57adac203fc7402c6bd75fc2
Lakhanya <lakhanya.mahlati@gmail.com>
2022-05-20 04:44:33 -0700

    fixed parentheses

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,10 +2,9 @@
 
 /**
  * __attribute__ ((constructor)) - invoke function before main
- * @premain: function before main
  */
 void __attribute__ ((constructor)) premain()
 {
-	printf("(You're beat! and yet, you must allow,)\n"
-			"(I bore my house upon my back!)\n");
+	printf("(You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!)\n");
 }
