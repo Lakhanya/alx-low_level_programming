a563c1822f3448626911e60f3bff90bb747d3f75
Lakhanya <lakhanya.mahlati@gmail.com>
2022-05-24 05:49:41 -0700

    Amended Task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,11 +8,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	const listint_t *c;
-	size_t count;
+	size_t nbr_nodes = 0;
+	const listint_t *current = h;
 
-	for (c = h, count = 0; c != NULL; count++, c = c->next)
-		printf("%d\n", c->n);
-
-	return (count);
+	while (current =! NULL)
+	{
+		printf("%i\n", current->n);
+		current = current->next;
+		nbr_nodes++;
+	}
+	return (nbr_nodes);
 }
