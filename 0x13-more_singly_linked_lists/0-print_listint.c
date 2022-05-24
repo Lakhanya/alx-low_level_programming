92bedb89091e5f72c8226bd4c61ef94c0d3e3b57
Lakhanya <lakhanya.mahlati@gmail.com>
2022-05-24 06:19:58 -0700

    Task 0 amendment

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,7 +11,7 @@ size_t print_listint(const listint_t *h)
 	size_t nbr_nodes = 0;
 	const listint_t *current = h;
 
-	while (current =! NULL)
+	while (current != NULL)
 	{
 		printf("%i\n", current->n);
 		current = current->next;
